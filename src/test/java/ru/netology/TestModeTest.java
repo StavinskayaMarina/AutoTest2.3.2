6dd0f758c3f25f6dbd39c9ce9a9ebeafac70afff
Stavinskaya_M <marina.st6277@gmail.com>
2023-07-27 23:18:56 +0600

    TestMode

--- src/test/java/ru/netology/TestModeTest.java
+++ src/test/java/ru/netology/TestModeTest.java
@@ -1,4 +1,76 @@
 package ru.netology;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class TestModeTest {
+
+    @BeforeEach
+    void createUrl() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+
+        var registeredUser = TestMode.Registration.getRegisteredUser("active");
+
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(".App_appContainer__3jRx1").shouldHave(exactText("  Личный кабинет"));
+    }
+
+    @Test
+    void shouldGetErrorIfNotRegisteredUser() {
+
+        var notRegisteredUser = TestMode.Registration.getUser("active");
+
+        $("[data-test-id=login] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id=password] input").setValue(notRegisteredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! Неверно указан логин или пароль"));
+
+    }
+
+    @Test
+    void shouldGetErrorIfBlockedUser() {
+
+        var blockedUser = TestMode.Registration.getRegisteredUser("blocked");
+
+        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! Пользователь заблокирован"));
+    }
+
+    @Test
+    void shouldGetErrorIfWrongLogin() {
+
+        var registeredUser = TestMode.Registration.getRegisteredUser("active");
+        var wrongLogin = TestMode.getRandomLogin();
+
+        $("[data-test-id=login] input").setValue(wrongLogin);
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! Неверно указан логин или пароль"));
+
+    }
+
+    @Test
+    void shouldGetErrorIfWrongPassword() {
+
+        var registeredUser = TestMode.Registration.getRegisteredUser("active");
+        var wrongPassword = TestMode.getRandomPassword();
+
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(wrongPassword);
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! Неверно указан логин или пароль"));
+    }
 }
+
